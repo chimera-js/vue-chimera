c37a4290f0170281ff59ae1f10b74b85cc8792f5
Sasan Farrokh <sasanfarrokh@Sasans-MacBook-Pro.local>
2018-08-17 17:05:55 +0430

    examples updated, docs updated

--- example/simple-2/src/App.vue
+++ example/simple-2/src/App.vue
@@ -1,68 +1,69 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1 v-if="$chimera.sasan.lastLoaded">
-      {{ $chimera.sasan.data.title }}
-    </h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
-  </div>
+    <div id="app">
+        <img src="./assets/logo.png">
+        <h1>
+            <span v-if="$chimera.simple.loading">Loading...</span>
+            <span v-if="$chimera.simple.lastLoaded">{{ $chimera.simple.data.title }}</span>
+        </h1>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+            <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+            <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+            <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+        </ul>
+        <h2>Ecosystem</h2>
+        <ul>
+            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+            <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
+            <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+        </ul>
+    </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
+    name: 'app',
 
     chimera: {
-      resources: {
-          sasan: 'https://idehhub.com/api/v1/blog/posts/10'
-      }
+        resources: {
+            simple: 'https://idehhub.com/api/v1/blog/posts/55?lang=en',
+        }
+    },
+
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        };
     }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
